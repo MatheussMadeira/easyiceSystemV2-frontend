20e61056c983d8c8a49ee72bf0d7a408ac004369
matheusmadeira <fumacammadeira@gmail.com>
2026-03-06 11:06:49 -0300

    updated api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "https://easyicesystemv2-backend.onrender.com/api", // Endereço do seu Node.js
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://easyicesystemv2-backend.onrender.com/api", // Endereço do seu Node.js
+  //baseURL: "http://localhost:3001/api",
 });
 
 export default api;
